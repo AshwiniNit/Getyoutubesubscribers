ef8366519acf8289f0fdfd6c85184a44d82b30b5
miss-ashwini <155818338+miss-ashwini@users.noreply.github.com>
2024-06-11 02:27:13 +0530

    initial status

---
+++ src/createDatabase.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const subscriberModel = require('./models/subscribers');
+const data = require('./data');
+require('dotenv').config();
+
+  const DATABASE_URL = "mongodb+srv://youtubesubscriber:user@cluster0.lrb6zff.mongodb.net/" || "mongodb://127.0.0.1:27017/user";
+//const DATABASE_URL = process.env.MONGODB_URL_ATLAS || process.env.MONGODB_URL_LOCAL;
+
+//Connecting to MongoDB using Mongoose
+mongoose.connect(DATABASE_URL, /*{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}*/);
+const db = mongoose.connection;
+
+//If an error occur during connection, handlr and log error
+db.on("error", (err) => console.log(err));
+
+//If connection is successful then log success message
+db.once("open", () => console.log("Database created..."));
+
+//Refresh all connections
+const refreshAll = async () => {
+    //Deleting previous data
+    await subscriberModel.deleteMany({})
+    //Inserting new data
+    await subscriberModel.insertMany(data)
+    //Disconnecting the database
+    await mongoose.disconnect();
+}
+refreshAll()
